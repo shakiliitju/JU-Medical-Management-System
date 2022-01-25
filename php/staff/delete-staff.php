55e6069232c19a1734df9cc6ffd21302ead78467
Amit Azim Amit <93789000+amitazim@users.noreply.github.com>
2022-01-25 12:27:10 +0600

    Doctor and Staff Delete

--- php/staff/delete-staff.php
+++ php/staff/delete-staff.php
@@ -4,10 +4,23 @@
 
 include 'db_conn.php';
 
-$D_ID = $_GET['D_ID'];
-$sql = " DELETE FROM doctor WHERE D_ID = $D_ID " ;
-mysqli_query($conn,$sql);
+// $D_ID = $_GET['D_ID'];
+// $sql = " DELETE FROM doctor WHERE D_ID = $D_ID " ;
+// mysqli_query($conn,$sql);
 
-header("location:doctor.php");
+// header("location:doctor.php");
+
+if(isset($_GET['S_ID'])){
+    $id = $_GET['S_ID'];
+
+    $sql = "delete from 'staff' where id=$id";
+    $result = mysqli_query($conn, $sql);
+    if($result){
+        echo "Deleted successfull";
+        header("location:staff.php");
+    }else{
+        die(mysqli_error($conn));
+    }
+}
 
 ?>
