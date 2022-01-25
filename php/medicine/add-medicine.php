920948ab25e57b536a113403ccab3474e86b117d
Md. Shakil Hossain <shakiljusc15@gmail.com>
2022-01-25 15:27:27 +0600

    update

---
+++ php/medicine/add-medicine.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ju Medical Center</title>
+    <link rel="shortcut icon" href="https://juniv.edu/images/favicon.ico">
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <!-- <link rel="stylesheet" href="/css/style.css"> -->
+    
+</head>
+
+<body>
+
+    <!-- header section starts  -->
+
+   
+
+    <!-- header section ends -->
+
+
+
+    <!-- booking section starts   -->
+
+    <section class="book" id="book">
+
+        <h1 class="heading">Add Medicine</h1>
+
+        <div class="row">
+            <form action="/php/medicine/add-medicine-con.php" method="post">
+                
+            <h2>Quantity</h2>
+                <input type="number" placeholder="Quantity" name="Quantity" class="box" required>
+
+
+                <input type="submit" value="Quantity" class="btn">
+            </form>
+
+        </div>
+
+    </section>
+
+    <!-- booking section ends -->
+
+
+
+
+    <!-- footer section starts  -->
+
+    
+
+    <!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
