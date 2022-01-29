04ad00e22a33db515eea9183814bf89d719d3713
Md. Shakil Hossain <shakiljusc15@gmail.com>
2022-01-29 09:10:19 +0600

    All search done

--- php/bed/add-bed-con.php
+++ php/bed/add-bed-con.php
@@ -3,13 +3,14 @@ session_start();
 include "db_conn.php";
 
 
-if (isset($_POST['Bed_Num']) && isset($_POST['Bed_Status'])) {
+if (isset($_POST['Bed_Num']) && isset($_POST['Date']) && isset($_POST['Bed_Status'])) {
 
     $Bed_Num = $_POST['Bed_Num'];
+    $Date = $_POST['Date'];
     $Bed_Status = $_POST['Bed_Status'];
    
 
-   $sql = "insert into bed(Bed_Num,Bed_Status) values('$Bed_Num','$Bed_Status')";
+   $sql = "insert into bed(Bed_Num,Date,Bed_Status) values('$Bed_Num','$Date','$Bed_Status')";
    mysqli_query($conn,$sql);
    header("location:bed.php");
 }
